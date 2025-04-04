731cda37859c356d2cdca55b0b37efe032d9d599
vish2285 <vishwas2285@gmail.com>
2025-04-03 22:31:32 -0700

    hid cursor on med and small screen

--- showcase/src/components/CustomCursor/index.js
+++ showcase/src/components/CustomCursor/index.js
@@ -4,35 +4,45 @@ import './index.scss'
 const CustomCursor = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 })
   const [innerPosition, setInnerPosition] = useState({ x: 0, y: 0 })
+  const [isVisible, setIsVisible] = useState(window.innerWidth > 768)
 
   useEffect(() => {
-    const moveCursor = (e) => {
-      setPosition({ x: e.clientX, y: e.clientY })
-      setTimeout(() => {
-        setInnerPosition({ x: e.clientX, y: e.clientY })
-      }, 50)
+    const handleResize = () => {
+      setIsVisible(window.innerWidth > 768)
     }
 
+    const moveCursor = (e) => {
+      if (isVisible) {
+        setPosition({ x: e.clientX, y: e.clientY })
+        setTimeout(() => {
+          setInnerPosition({ x: e.clientX, y: e.clientY })
+        }, 50)
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
     document.addEventListener('mousemove', moveCursor)
-    return () => document.removeEventListener('mousemove', moveCursor)
-  }, [])
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+      document.removeEventListener('mousemove', moveCursor)
+    }
+  }, [isVisible])
+
+  if (!isVisible) return null
 
   return (
     <>
-      {/* Outer Circle */}
       <div
         className="custom-cursor-outer"
         style={{
           transform: `translate(${position.x - 24}px, ${position.y - 24}px)`,
         }}
       />
-      {/* Inner Circle */}
       <div
         className="custom-cursor-inner"
         style={{
-          transform: `translate(${innerPosition.x - 8}px, ${
-            innerPosition.y - 8
-          }px)`,
+          transform: `translate(${innerPosition.x - 8}px, ${innerPosition.y - 8}px)`,
         }}
       />
     </>
