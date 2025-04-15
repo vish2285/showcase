9df29e28d9681c35bd49b8ed057aa75d7ba7c389
Vish <155484823+vish2285@users.noreply.github.com>
2025-04-15 14:04:56 -0700

    Update index.js

--- showcase/src/components/Portfolio/index.js
+++ showcase/src/components/Portfolio/index.js
@@ -1,30 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react"; 
 import "./index.scss";
 import AnimatedLetters from "../AnimatedLetters";
 import portfolioData from "../../data/portfolio.json";
 
 const Portfolio = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
-    console.log(portfolioData);
 
     useEffect(() => {
         const timer = setTimeout(() => {
             setLetterClass("text-animate-hover");
         }, 3000); 
 
-        return () => { 
-            clearTimeout(timer);
-        }
-    });
+        return () => clearTimeout(timer);
+    }, []);
 
     const renderPortfolio = (portfolio) => { 
         return (
-            <div className = "images-container">
+            <div className="images-container">
                 {
                     portfolio.map((port, idx) => {
                         return (
-                            <div key={idx} className="image-box">
-                                <img src={port.cover} alt="portfolio" className = "portfolio-image"/>
+                            <div
+                                key={idx}
+                                className="image-box fade-up"
+                                style={{ animationDelay: `${idx * 0.2}s` }}
+                            >
+                                <img src={port.cover} alt="portfolio" className="portfolio-image" />
                                 <div className="content">
                                     <p className="title">{port.title}</p>
                                     <h4 className="description">{port.description}</h4>
@@ -35,18 +36,21 @@ const Portfolio = () => {
                     })
                 }
             </div>
-        )
-    }
-    return (
-        <>
-            <div className="container portfolio-page">
-                <h1 className = "page-title">
-                    <AnimatedLetters letterClass = {letterClass} strArray={"Portfolio".split("")} idx={15}/>
-                </h1>
-                <div>{renderPortfolio(portfolioData.portfolio)}</div>
-            </div>
-        </>
-    );
-}
+        );
+    };
 
-export default Portfolio
+    return (
+        <div className="container portfolio-page">
+            <h1 className="page-title">
+                <AnimatedLetters
+                    letterClass={letterClass}
+                    strArray={"Portfolio".split("")}
+                    idx={15}
+                />
+            </h1>
+            <div>{renderPortfolio(portfolioData.portfolio)}</div>
+        </div>
+    );
+};
+
+export default Portfolio;
