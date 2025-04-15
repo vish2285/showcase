fa800f3deb7b1a3f51ed4752bb24297d43160369
Vish <155484823+vish2285@users.noreply.github.com>
2025-04-15 13:59:03 -0700

    Update index.js

--- showcase/src/components/Layout/index.js
+++ showcase/src/components/Layout/index.js
@@ -1,26 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import Sidebar from '../Sidebar/'
 import CustomCursor from '../CustomCursor';
-
 import './index.scss'
 
-const Layout = () => {
-  return (
-    <div className="App">
-      <Sidebar />
-      <CustomCursor />
-      <div className="page">
-        <span className="tags top-tags">&lt;body&gt;</span>
-
-        <Outlet />
-        <span className="tags bottom-tags">
-          &lt;/body&gt;
-          <br />
-          <span className="bottom-tag-html">&lt;/html&gt;</span>
-        </span>
-      </div>
-    </div>
-  )
+const Layout = () => { 
+    return(
+        <div className = "App">
+            <Sidebar />
+            <CustomCursor />
+            <div className = "page">
+                <Outlet />
+            </div>
+        </div>
+    ) 
 }
 
-export default Layout;
+export default Layout
