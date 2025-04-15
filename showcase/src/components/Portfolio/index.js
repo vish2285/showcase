2b4ea8519ac315da01a45e37af64f531371d815a
Vish <155484823+vish2285@users.noreply.github.com>
2025-04-15 12:40:57 -0700

    Update index.js

--- showcase/src/components/Portfolio/index.js
+++ showcase/src/components/Portfolio/index.js
@@ -1,60 +1,54 @@
-import React from 'react'
-import './index.scss'
-import Loader from 'react-loaders'
-import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
-import portfolioData from '../../data/portfolio.json'
+import React, { useEffect, useState } from "react";
+import Loader from "react-loaders";
+import "./index.scss";
+import AnimatedLetters from "../AnimatedLetters";
+import portfolioData from "../../data/portfolio.json";
 
 const Portfolio = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
+    const [letterClass, setLetterClass] = useState('text-animate');
+    console.log(portfolioData);
 
-  const renderPortfolio = (portfolio) => {
-    return (
-      <div className="images-container">
-        {portfolio.map((port, idx) => {
-          return (
-            <div className="image-box" key={idx}>
-              <img
-                src={port.cover}
-                className="portfolio-image"
-                alt="portfolio"
-              />
-              <div className="content">
-                <p className="title">{port.name}</p>
-                <h4 className="description">{port.title}</h4>
-                <button className="btn" onClick={() => window.open(port.url)}>
-                  View
-                </button>
-              </div>
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLetterClass("text-animate-hover");
+        }, 3000); 
+
+        return () => { 
+            clearTimeout(timer);
+        }
+    });
+
+    const renderPortfolio = (portfolio) => { 
+        return (
+            <div className = "images-container">
+                {
+                    portfolio.map((port, idx) => {
+                        return (
+                            <div key={idx} className="image-box">
+                                <img src={port.cover} alt="portfolio" className = "portfolio-image"/>
+                                <div className="content">
+                                    <p className="title">{port.title}</p>
+                                    <h4 className="description">{port.description}</h4>
+                                    <button className="btn" onClick={() => window.open(port.url)}>View</button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
             </div>
-          )
-        })}
-      </div>
-    )
-  }
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 4000)
-
-    return () => clearTimeout(timeoutId)
-  }, [])
-
-  return (
-    <>
-      <div className="container portfolio-page">
-        <h1 className="page-title">
-          <AnimatedLetters
-            letterClass={letterClass}
-            strArray={'Projects'.split('')}
-            idx={15}
-          />
-        </h1>
-        <div>{renderPortfolio(portfolioData.portfolio)}</div>
-      </div>
-      <Loader type="pacman" />
-    </>
-  )
+        )
+    }
+    return (
+        <>
+            <div className="container portfolio-page">
+                <h1 className = "page-title">
+                    <AnimatedLetters letterClass = {letterClass} strArray={"Portfolio".split("")} idx={15}/>
+                </h1>
+                <div>{renderPortfolio(portfolioData.portfolio)}</div>
+            </div>
+            <Loader type="pacman"/>
+        </>
+    );
 }
 
 export default Portfolio
