e17b2a53cbac21a52a39a1f45d9e9cfd5c45a036
Vish <155484823+vish2285@users.noreply.github.com>
2025-04-29 22:54:40 +0000

    Changes to Home

--- showcase/src/components/Home/index.js
+++ showcase/src/components/Home/index.js
@@ -1,16 +1,16 @@
-import { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Link } from 'react-router-dom'
-import Typed from 'typed.js';
-import React from 'react';
+import Typed from 'typed.js'
+import Loader from 'react-loaders'
 import LogoV from '../../assets/images/v-logo.svg'
 import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
-import Logo from './Logo';
+import Logo from './Logo'
 
 const Home = () => {
-  const el = React.useRef(null)
+  const el = useRef(null)
 
-  React.useEffect(() => {
+  useEffect(() => {
     const typed = new Typed(el.current, {
       strings: [
         'Software Engineer',
@@ -23,45 +23,49 @@ const Home = () => {
       backDelay: 1000,
       loop: true,
     })
-
-    return () => {
-      typed.destroy()
-    }
+    return () => typed.destroy()
   }, [])
 
-
-  const [letterClass, setLetterClass] = useState('text-animate');
-  const nameArray = ['i','s','h','w','a','s']
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['i', 's', 'h', 'w', 'a', 's']
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setLetterClass('text-animate-hover');
-    }, 4000);
-  
-    return () => clearTimeout(timeoutId); // Proper cleanup
-  }, []);
+    const timerId = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+    return () => clearTimeout(timerId)
+  }, [])
 
   return (
-    <div className="container home-page">
-      <div className= "text-zone">
-        <h1>
-          <span className={letterClass}>H</span>
-          <span className={`${letterClass} _12`}>i,</span>
-          <br />
-          <span className={`${letterClass} _13`}>I</span>
-          <span className={`${letterClass} _14`}>'m &nbsp;</span>
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={`${letterClass} _8`}>H</span>
+            <span className={`${letterClass} _9`}>i,</span>
+            <br />
+            <span className={`${letterClass} _10`}>I</span>
+            <span className={`${letterClass} _11`}>'m&nbsp;</span>
             <img src={LogoV} alt="developer" />
-        <AnimatedLetters letterClass = {letterClass} strArray ={nameArray} idx= {14}/><br />
-          <span id ="typed">
-            {/* Reference the span for Typed.js */}
-            <span ref={el}></span>
-          </span>
-        </h1>
-        <Link to="/contact" className="flat-button">CONTACT ME</Link>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={12}
+            />
+            <br />
+            <span id="typed">
+              <span ref={el}></span>
+            </span>
+          </h1>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo className="Home" />
       </div>
-      <Logo />
-    </div>
-  );
+      <Loader type="pacman" />
+    </>
+  )
 }
 
 export default Home
