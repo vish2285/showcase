00f53209fd6b6d65b79abd57e633bb8c2c031b18
Vish <155484823+vish2285@users.noreply.github.com>
2025-04-29 23:08:56 +0000

    Testing

--- showcase/src/components/Home/Logo/index.js
+++ showcase/src/components/Home/Logo/index.js
@@ -6,16 +6,6 @@ const Logo = () => {
   return (
     <div className="logo-container">
       <img className="solid-logo animate" src={LogoV} alt="JavaScript, Developer" />
-      <svg
-        version="1.0"
-        xmlns="http://www.w3.org/2000/svg"
-        width="274.000000pt"
-        height="230.000000pt"
-        viewBox="0 0 274.000000 230.000000"
-        preserveAspectRatio="xMidYMid meet"
-        className="svg-container"
-      >
-      </svg>
     </div>
   );
 };
--- showcase/src/components/Home/index.js
+++ showcase/src/components/Home/index.js
@@ -1,71 +1,49 @@
-import React, { useEffect, useRef, useState } from 'react'
-import { Link } from 'react-router-dom'
-import Typed from 'typed.js'
-import Loader from 'react-loaders'
-import LogoV from '../../assets/images/v-logo.svg'
-import AnimatedLetters from '../AnimatedLetters'
-import './index.scss'
-import Logo from './Logo'
+import { useEffect, useState } from 'react';
+import Loader from 'react-loaders';
+import { Link } from 'react-router-dom';
+import LogoV from '../../assets/images/v-logo.svg';
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+import Logo from './Logo';
 
 const Home = () => {
-  const el = useRef(null)
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = "ishwas, ".split("");
+    const jobArray = "software engineer.".split("");
 
-  useEffect(() => {
-    const typed = new Typed(el.current, {
-      strings: [
-        'Software Engineer',
-        'Student',
-        'Only Child',
-        'Sophomore in College',
-      ],
-      typeSpeed: 50,
-      backSpeed: 50,
-      backDelay: 1000,
-      loop: true,
-    })
-    return () => typed.destroy()
-  }, [])
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+          setLetterClass('text-animate-hover');
+        }, 4000);
+      
+        return () => {
+          clearTimeout(timerId);
+        };
+      }, []);
 
-  const [letterClass, setLetterClass] = useState('text-animate')
-  const nameArray = ['i', 's', 'h', 'w', 'a', 's']
-
-  useEffect(() => {
-    const timerId = setTimeout(() => {
-      setLetterClass('text-animate-hover')
-    }, 4000)
-    return () => clearTimeout(timerId)
-  }, [])
-
-  return (
-    <>
-      <div className="container home-page">
-        <div className="text-zone">
-          <h1>
-            <span className={`${letterClass} _8`}>H</span>
-            <span className={`${letterClass} _9`}>i,</span>
-            <br />
-            <span className={`${letterClass} _10`}>I</span>
-            <span className={`${letterClass} _11`}>'m&nbsp;</span>
-            <img src={LogoV} alt="developer" />
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={nameArray}
-              idx={12}
-            />
-            <br />
-            <span id="typed">
-              <span ref={el}></span>
-            </span>
-          </h1>
-          <Link to="/contact" className="flat-button">
-            CONTACT ME
-          </Link>
+    return(
+      <>
+        <div className = "container home-page">
+            <div className="text-zone">
+                <h1>
+                <span className = {`${letterClass} _8`}>H</span>
+                <span className = {`${letterClass} _9`}>i</span>
+                <br /> 
+                <span className = {`${letterClass} _10`}>I</span>
+                <span className = {`${letterClass} _11`}>'m</span>
+                <img src={LogoV} alt = "developer" />
+                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={12} />
+                <br /> 
+                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={15} /> 
+                </h1>
+                <h2>Computer Science & AI, 2027 at UC Irvine</h2>
+                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+            </div>
+            <Logo className="Home"/>
         </div>
-        <Logo className="Home" />
-      </div>
-      <Loader type="pacman" />
-    </>
-  )
+        <Loader type="pacman" />
+      </>
+    )
 }
 
 export default Home
